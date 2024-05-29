44e8fba5b39c5069e77be0df2b314fc38be053eb
Oana Poran <oanaporandev@gmail.com>
2024-05-29 12:46:57 +0300

    autocomplete - project init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {AutocompleteComponent} from "./components/autocomplete/autocomplete.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AutocompleteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'untitled';
+  title = 'Autocomplete with Angular';
 }
---
+++ src/app/components/autocomplete/autocomplete.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'autocomplete',
+  standalone: true,
+  imports: [],
+  templateUrl: './autocomplete.component.html',
+  styleUrl: './autocomplete.component.css'
+})
+export class AutocompleteComponent {
+
+}
